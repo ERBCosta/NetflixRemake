e4ced74369010dc3dece11550045d26b92ce1385
ERBCosta <eltonr@gmail.com>
2023-09-15 15:32:19 -0300

    Quarto commit! Separando as categorias de modelos utilizando o padrão MVC. E deixando o adapter dinâmico para a listagem de filmes.

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainAdapter.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainAdapter.kt
@@ -3,10 +3,12 @@ package co.tiagoaguiar.netflixremake
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Movie
 
 //MainAdapter é o adaptador da tela principal e filha do RecyclerViiew.Adapater
-class MainAdapter : RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
+class MainAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
 
     //onCreateViewHolder - onBindViewHolder - getItemCount 3 itens obrigatórios para o RecyclerView
 
@@ -21,14 +23,20 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         //Esse método é disparado sempre que houver rolagem
+        val movie = movies[position]
+        holder.bind(movie)
     }
 
     override fun getItemCount(): Int {
-        return 60
+        return movies.size
     }
 
     //MovieViewHolder é a célula do filme e herda as propriedades do RecyclerView.ViewHolder
     inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) {
+            val txtTest: TextView = itemView.findViewById(R.id.txt_test)
+            txtTest.text = movie.coverUrl
+        }
 
     }
 
